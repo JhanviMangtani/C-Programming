cdd545097d45a37ab0639b131b5e0834043dde1d
JhanviMangtani <jhanvimangtani@gmail.com>
2025-09-04 00:07:07 +0530

    Added programs using loops

---
+++ Loops/numbers_in_sequence(for).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+int i,n;
+printf("Enter number till which sequence is needed\n");
+scanf("%d",&n);
+
+printf("Sequence of numbers is : \n");
+for (i=0;i<=n;i++)
+{
+printf("%d\n",i);
+}
+
+return 0;
+ 
+}
